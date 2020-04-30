fbf57b84a52be111183052e167959a44adb5dd33
liamst19 <liamtstko20@gmail.com>
2020-04-30 15:06:07 +0900

    Completed Exercise 1.4: step 4

--- part1/course-information/src/index.js
+++ part1/course-information/src/index.js
@@ -5,20 +5,23 @@ const Header = props => {
   return <h1>{props.course}</h1>
 }
 
-const Part = props => {
-  return <p>{props.name} {props.exercises}</p>
+const Part = ({ name, exercises }) => {
+  return <p>{name} {exercises}</p>
 }
 
 const Content = ({ parts }) => {
-  return (<div>
-            <Part name={parts[0].name} exercises={ parts[0].exercises }/>
-            <Part name={parts[1].name} exercises={ parts[1].exercises }/>
-            <Part name={parts[2].name} exercises={ parts[2].exercises }/>
-          </div>)
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={ parts[0].exercises }/>
+      <Part name={parts[1].name} exercises={ parts[1].exercises }/>
+      <Part name={parts[2].name} exercises={ parts[2].exercises }/>
+    </div>
+  )
 }
 
-const Total = props => {
-  return <p>Number of exercises { props.exerciseCount }</p>
+const Total = ({ parts })=> {
+  const total = parts.reduce((n, p) => n + p.exercises, 0)
+  return <p>Number of exercises { total }</p>
 }
 
 const App = () => {
@@ -28,12 +31,11 @@ const App = () => {
     { name: 'Using props to pass data', exercises: 7 },
     { name: 'State of a component',     exercises: 14 }
   ]
-  const exerciseCount = parts.reduce((n, p) => n + p.exercises, 0)
 
   return (<div>
-            <Header course={ course }/>
+            <Header  course={ course }/>
             <Content parts={ parts }/>
-            <Total exerciseCount={ exerciseCount }/>
+            <Total   parts={ parts }/>
           </div>)
 }
 
