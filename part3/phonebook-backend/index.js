a98c41cd650b17fe6c658e0e15d73889c7475ca7
liamst19 <liamtstko20@gmail.com>
2020-04-30 22:13:24 +0900

    Completed Exercise 3.2: Phonebook backend step2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -43,6 +43,10 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>${new Date().toString()}<p><p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
