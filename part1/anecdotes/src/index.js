5bb171e34fd2bc0341a42bbe460022bdab36633b
liamst19 <liamtstko20@gmail.com>
2020-04-30 15:55:51 +0900

    Completed exercises 1.12-1.14: anecdotes

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const Votes = (props) => {
+    return (<p>has {props.votes} votes</p>);
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0]);
+
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  const incrementVote = (i) => {
+    setVotes(votes.map((v, vi)=>{
+        if(vi === i)
+            return v+1;
+        else
+            return v;
+    }));
+  };
+
+  return (
+    <div>
+        <div>
+            <h2>Anecdote of the day</h2>
+            <p>{props.anecdotes[selected]}</p>
+            <Votes votes={votes[selected]} />
+            <div>
+                <button onClick={()=>{ incrementVote(selected) }}>vote</button>
+                <button onClick={()=>{ setSelected(getRandomInt(anecdotes.length)) }}>next anecdote</button>
+            </div>
+        </div>
+        <div>
+            <h2>Anecdote with the most votes</h2>
+            <p>{props.anecdotes[votes.indexOf(Math.max.apply(Math, votes))]}</p>
+        </div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
