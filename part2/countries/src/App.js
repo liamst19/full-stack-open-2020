e5d6c3698108a4a325f38656d526e63fed04c85a
liamst19 <liamtstko20@gmail.com>
2020-04-30 18:06:08 +0900

    Completed Excersise 2.12-13: Data for countries skipping 2.14 weather data because I'd have to register for an API key.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,85 +1,106 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-  const FilterCountries = props => {
-    return (<div>
+const FilterCountries = props => {
+  return (
+    <div>
       find countries <input value={props.filter} onChange={e => props.handleChange(e.target.value)} />
-    </div>)
-  }
+    </div>
+  )
+}
 
-  const Country = props => {
-    return (<li>
+const Country = props => {
+  return (
+    <li>
       {props.country.name}
       <button onClick={e => props.showHandler(props.country) }>show</button>
-    </li>)
-  }
+    </li>
+  )
+}
 
-  const CapitalWeather = props => {
+const CapitalWeather = props => {
 
-  return (<div><h3>{`Weather in ${props.country.capital}`}</h3>
-  
-  </div>);
-  }
+  return (
+    <div>
+      <h3>{`Weather in ${props.country.capital}`}</h3>
+    </div>
+  );
+}
 
-  const CountryDetails = props => {
+const CountryDetails = props => {
 
-    return (<div>
+  return (
+    <div>
       <h1>{props.country.name}</h1>
       <div>capital {props.country.capital}</div>
       <div>population {props.country.population}</div>
       <div><h3>languages</h3>
-      <ul>
-        {props.country.languages.map(lang => <li key={lang.iso639_1}>{lang.name}</li>)}
-      </ul>
+        <ul>
+          {props.country.languages.map(lang => <li key={lang.iso639_1}>{lang.name}</li>)}
+        </ul>
       </div>
-      <div><img src={props.country.flag} alt={`flag of ${props.country.name}`} width="200px" /></div>
+      <div>
+        <img
+          src={props.country.flag}
+          alt={`flag of ${props.country.name}`}
+          width="200px" />
+      </div>
       <CapitalWeather country={props.country} />
-    </div>);
+    </div>
+  );
+}
+
+const Results = props => {
+  const maxCountries = 10;
+
+  if (props.countries.length > maxCountries){
+    // Too many results
+    return (<div>Too many matches, specify another filter</div>);
+  }
+  else if(props.countries.length === 1){
+    // Show details
+    console.log("Showing details to ", props.countries[0].name);
+    return (<CountryDetails country={props.countries[0]} />);
   }
 
-  const Results = props => {
-    const maxCountries = 10;
-
-    if (props.countries.length > maxCountries){
-      // Too many results
-      return (<div>Too many matches, specify another filter</div>);
-    }
-    else if(props.countries.length === 1){
-      // Show details
-      console.log("Showing details to ", props.countries[0].name);
-      return (<CountryDetails country={props.countries[0]} />);
-    }
-
-    // Show list
-    return (<div>
+  // Show list
+  return (
+    <div>
       <ul>
         { props.countries.map(country => <Country key={country.name} country={country} showHandler={props.showHandler} />) }
       </ul>
-    </div>)
-  }
+    </div>
+  )
+}
 
-  function App() {
-    const [countries, setCountries] = useState([]);
-    const [countryFilter, setCountryFilter] = useState('');
-    const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(countryFilter.toLowerCase()));
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [countryFilter, setCountryFilter] = useState('');
+  const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(countryFilter.toLowerCase()));
 
-    const showHandler = country => {
-      setCountryFilter(country.name.trim());
-    };
+  const showHandler = country => {
+    setCountryFilter(country.name.trim());
+  };
 
-    // Get list of countries
-    useEffect(()=>{
-      axios
-        .get('https://restcountries.eu/rest/v2/all')
-        .then(response =>{
-          setCountries(response.data);
-        });
-    }, []);
+  // Get list of countries
+  useEffect(()=>{
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response =>{
+        setCountries(response.data);
+      });
+  }, []);
 
-  return (<div>
-    <FilterCountries filter={countryFilter} handleChange={setCountryFilter} />
-    <Results countries={filteredCountries} showHandler={showHandler} />
-  </div>);
+  return (
+    <div>
+      <FilterCountries
+        filter={countryFilter}
+        handleChange={setCountryFilter} />
+      <Results
+        countries={filteredCountries}
+        showHandler={showHandler} />
+    </div>
+  );
 }
 
 export default App;
