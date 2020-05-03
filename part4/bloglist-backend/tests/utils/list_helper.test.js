16add2dfab845c223ae9a80140ba2ef14e754fe9
liamst19 <liamtstko20@gmail.com>
2020-05-03 15:11:53 +0900

    Completed Exercise 4.5: helper functions and unit tests, step3

--- part4/bloglist-backend/tests/utils/list_helper.test.js
+++ part4/bloglist-backend/tests/utils/list_helper.test.js
@@ -1,8 +1,46 @@
 const listHelper = require('../../utils/list_helper')
+const { listWithOneBlog, listWithManyBlogs } = require('../test_helper')
 
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    expect(result).toBe(36)
+  })
+})
+
+describe('favorite blog', () => {
+
+  test('of empty list is null', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toBeNull()
+  })
+
+  test('when list has only one blog favorite blog is that one', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toBe(listWithOneBlog[0])
+  })
+
+  test('of a bigger list is evaluated right', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    expect(result).toBe(listWithManyBlogs[2])
+  })
+
 })
--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -1,8 +1,17 @@
 
-const dummy = blogs => {
+const dummy = () => {
   return 1
+}
+const totalLikes = blogs => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = blogs => {
+  return blogs.reduce((fav, blog) => fav && fav.likes > blog.likes ? fav: blog, null)
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
