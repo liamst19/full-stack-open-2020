e23ea394146b581ae051a027cbb057d8dc1ab5b1
liamst19 <liamtstko20@gmail.com>
2020-05-11 23:33:36 +0900

    Refactorin unicafe proj

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,47 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import reducer from './reducer'
+
+import App from './App'
 
 const store = createStore(reducer)
 
-const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
-  const neutral = () => {
-    store.dispatch({
-      type: 'OK'
-    })
-  }
-  const bad = () => {
-    store.dispatch({
-      type: 'BAD'
-    })
-  }
-  const reset = () => {
-    store.dispatch({
-      type: 'ZERO'
-    })
-  }
-
-  return (
-    <div>
-      <button onClick={good}>good</button> 
-      <button onClick={neutral}>neutral</button>
-      <button onClick={bad}>bad</button>
-      <button onClick={reset}>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral {store.getState().ok}</div>
-      <div>bad{store.getState().bad}</div>
-    </div>
-  )
-}
 
 const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root'))
 }
 
 renderApp()
