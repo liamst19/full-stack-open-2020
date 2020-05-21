4c33b5c74b5e44f3a5923f021b510a545ee8d2e4
liamst19 <liamtstko20@gmail.com>
2020-05-21 12:22:45 +0900

    Completed Exercise 8.8: Authors view

---
+++ part8/library-frontend/src/queries.js
@@ -0,0 +1,50 @@
+import { gql } from '@apollo/client'
+
+export const ALL_AUTHORS = gql`
+query {
+  allAuthors {
+    name
+    born
+    bookCount
+  }
+}
+`
+
+// export const ALL_BOOKS = gql`
+// query {
+//   allBooks {
+//     title
+//     author
+//     published
+//     genres
+//   }
+// }
+// `
+
+// export const ADD_BOOK = gql`
+// mutation {
+//   addBook (
+//     $title: String!
+//     $author: String!
+//     $published: Int!
+//     $genres: [String]
+//   ) {
+//     title
+//     author
+//     published
+//     genres
+//   }
+// }
+// `
+
+// export const EDIT_AUTHOR = gql`
+// mutation {
+//   editAuthor(
+//     $name: String!
+//     $setBornTo: Int!
+//   ) {
+//     name
+//     born
+//   }
+// }
+// `
