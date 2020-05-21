71fb7c9039fb2ba38d68329b088b4cd89e8fe989
liamst19 <liamtstko20@gmail.com>
2020-05-21 23:18:24 +0900

    Completed Exercise 8.13: Database, part 1

---
+++ part8/library-backend/gql/typedefs.js
@@ -0,0 +1,40 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Author {
+    id: ID!
+    name: String!
+    born: Int
+    bookCount: Int
+  }
+  type Book{
+    id: ID!
+    title: String!
+    author: Author
+    published: Int!
+    genres: [String]!
+  }
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(
+      author: String
+      genre: String
+    ): [Book!]!
+    allAuthors: [Author!]!
+  }
+  type Mutation {
+    addBook (
+        title: String!
+        author: String
+        published: Int!
+        genres: [String]
+    ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
+  }
+`
+
+module.exports = typeDefs
--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -1,126 +1,20 @@
-const { ApolloServer, gql } = require('apollo-server')
+require('dotenv').config()
 const { v1: uuid } = require('uuid')
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
+const { ApolloServer, gql } = require('apollo-server')
+const mongoose = require('mongoose')
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+const typeDefs = require('./gql/typedefs')
+const Book = require('./models/book')
+const Author = require('./models/author')
 
-const typeDefs = gql`
-  type Author {
-    id: ID!
-    name: String!
-    born: Int
-    bookCount: Int
-  }
-  type Book{
-    id: ID!
-    title: String!
-    author: String!
-    published: Int!
-    genres: [String]!
-  }
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-    allBooks(
-      author: String
-      genre: String
-    ): [Book!]!
-    allAuthors: [Author!]!
-  }
-  type Mutation {
-    addBook (
-        title: String!
-        author: String!
-        published: Int!
-        genres: [String]
-    ): Book
-    editAuthor(
-      name: String!
-      setBornTo: Int!
-    ): Author
-  }
-`
+const { authors, books } = require('./models/sampledata')
 
 const resolvers = {
   Book: root => ({
     id: root => root.id,
     title: root => root.title,
-    author: root => root.name,
+    author: root => authors.author,
     published: root => root.published,
     genres: root => root.genres
   }),
@@ -131,30 +25,14 @@ const resolvers = {
     bookCount: root => root.bookCount
   }),
   Query: {
-    bookCount: () => books.length,
-    authorCount: () =>authors.length,
-    allBooks: (root, args) =>  books.filter(book =>{
-        return (!args.author || (args.author && args.author === book.author))
-          && (!args.genre || (args.genre && book.genres.some(genre => genre === args.genre)))
-      }),
-    allAuthors: () => authors.map(author => ({...author, bookCount: books.filter(book => book.author === author.name).length}))
+    bookCount: () => Book.count({}),
+    authorCount: () => Author.count({}),
+    allBooks: (root, args) => Book.find({...args}),
+    allAuthors: () => Author.find({})
   },
   Mutation: {
-    addBook: (root, args) => {
-      // Add author if it does not already exist
-      if(!authors.some(author => author.name === args.author)){
-        const author = {
-          id: uuid(),
-          name: args.author
-        }
-        authors = authors.concat(author)
-      }
-      const book = {...args,
-                    genres: args.genres ? args.genres : [],
-                    id: uuid()}
-      books = books.concat(book)
-      return book
-    },
+    addBook: (root, args) =>
+      (new Book({...args, author: null})).save(),
     editAuthor: (root, args) => {
       const author = authors.filter(author => author.name.toLowerCase() === args.name.toLowerCase())[0]
       if(!author) return null
@@ -166,6 +44,25 @@ const resolvers = {
     }
   }
 }
+
+// Database
+console.log(`Connecting to database ${process.env.MONGODB_URI} ...`)
+mongoose
+  .connect(
+    process.env.MONGODB_URI,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    }
+  )
+  .then(() => {
+    console.log('...Connected to database')
+  })
+  .catch(error => {
+    console.log('...Error connecting to database:', error.message)
+  })
 
 const server = new ApolloServer({
   typeDefs,
