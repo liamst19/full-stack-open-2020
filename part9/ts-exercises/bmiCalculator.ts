04b1efa03457777bc75a3cbf5e2bcb2545bb25b5
liamst19 <liamtstko20@gmail.com>
2020-05-26 13:49:01 +0900

    Completed Exercises 9.1-9.2: Body mass index and Exercise Calculator

---
+++ part9/ts-exercises/bmiCalculator.ts
@@ -0,0 +1,56 @@
+
+/* Exercise 9.1 Body mass index
+Write a function calculateBmi that counts BMI based on given weight
+(in kilograms) and height (in centimeters) and then returns a message
+that suits the results.
+*/
+
+const calculateBmi = (height: number, weight: number): string => {
+    // The BMI is universally expressed in kg/m2, resulting from mass
+    // in kilograms and height in metres.
+    const heightM = height / 100
+    const bmi = weight / (heightM * heightM)
+
+    if (bmi < 0 || bmi > 100) {
+        throw Error('calculated bmi is off the scale')
+    }
+
+    const bmiScale = [
+        {
+            text: 'Very severely underweight',
+            from: 0, to: 15
+        },
+        {
+            text: 'Severely underweight',
+            from: 15, to: 16
+        },
+        {
+            text: 'Underweight',
+            from: 16, to: 18.5
+        },
+        {
+            text: 'Normal (healthy weight)',
+            from: 18.5, to: 25
+        },
+        {
+            text: 'Overweight',
+            from: 25, to: 30
+        },
+        {
+            text: 'Obese Class I (Moderately obese)',
+            from: 30, to: 35
+        },
+        {
+            text: 'Obese Class II (Severely obese)',
+            from: 35, to: 40
+        },
+        {
+            text: 'Obese Class III (Very severely obese)',
+            from: 40, to: 100
+        }
+    ]
+
+    return bmiScale.filter(c => bmi > c.from && bmi < c.to)[0].text
+}
+
+console.log(calculateBmi(180, 74))
---
+++ part9/ts-exercises/exerciseCalculator.ts
@@ -0,0 +1,115 @@
+/* Exercise 9.2 Exercise calculator
+
+   Write a function calculateExercises that calculates the average time
+   of daily exercise hours and compares it to the target amount of daily
+   hours and returns an object that includes the following values:
+
+     - the number of days
+     - the number of training days
+     - the original target value
+     - the calculated average time
+     - boolean value describing if the target was reached
+     - a rating between the numbers 1-3 that tells how well the hours are
+       met. You can decide on the metric on your own.
+     - a text value explaining the rating
+
+   The daily exercise hours are given to the function as an array that
+   contains the number of exercise hours for each day in the training
+   period. Eg. a week with 3 hours of training at Monday, none at
+   Tuesday, 2 hours at Wednesday, 4.5 hours at Thursday and so on would
+   be represented by the following array:
+
+     [3, 0, 2, 4.5, 0, 3, 1]
+
+   If you would call the function with parameters [3, 0, 2, 4.5, 0, 3, 1]
+   and 2 it could return
+
+   { periodLength: 7,
+     trainingDays: 5,
+     success: false,
+     rating: 2,
+     ratingDescription: 'not too bad but could be better',
+     target: 2,
+     average: 1.9285714285714286 }
+
+*/
+
+interface ExerciseStats {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
+}
+
+interface Rating {
+    rating: number,
+    ratingDescription: string
+}
+
+function getRating(average: number, target: number): Rating {
+
+    const min = -100
+    const max = 100
+
+    const difference = average - target
+    if (difference < min || difference > max) {
+        return {
+            rating: 0,
+            ratingDescription: 'you should reconsider your target or there is something wrong with the data'
+        }
+    }
+
+    const ratingScale = [
+        {
+            rating: 1,
+            description: 'Perhaps you should set a more achievable goal',
+            from: min, to: -10
+        },
+        {
+            rating: 2,
+            description: 'Come on, you can do better!',
+            from: -10, to: 0
+        },
+        {
+            rating: 3,
+            description: 'You\'re right on your mark, keep at it',
+            from: 0, to: 5
+        },
+        {
+            rating: 4,
+            description: 'You\'re doing terrific',
+            from: 5, to: 10
+        },
+        {
+            rating: 5,
+            description: 'You\'re making it too easy for yourself',
+            from: 10, to: max
+        }
+    ]
+
+    const rating = ratingScale.filter(s => difference >= s.from && difference <= s.to)[0]
+    return {
+        rating: rating.rating,
+        ratingDescription: rating.description
+    }
+}
+
+function calculateExercise(data: number[], target: number): ExerciseStats {
+
+    const average = data.reduce((sum, d) => sum + d, 0) / data.length
+
+    return {
+        target,
+        average,
+        success: average > target,
+        periodLength: data.length,
+        trainingDays: data.filter(d => d > 0).length,
+        ...getRating(average, target)
+    }
+}
+
+
+console.log(calculateExercise([3, 0, 2, 4.5, 0, 3, 1], 2))
