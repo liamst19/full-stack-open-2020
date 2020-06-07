2c4b042fb2d71612c4f67015a18dc4dffea52480
liamst19 <liamtstko20@gmail.com>
2020-06-07 22:27:00 +0900

    Completed Exercise 9.15

--- part9/course-information-typescript/src/components/Content.tsx
+++ part9/course-information-typescript/src/components/Content.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { CoursePart } from '../types';
+import { CoursePart, assertNever } from '../types';
 
 const Content: React.FC<{ courseParts: CoursePart[] }> = ({ courseParts }) => {
   return (
     <ul>
-      { courseParts.map(part => (
-        <li key={part.name}>
-          {part.name} {part.exerciseCount}
-        </li>
-        ))}
+      { courseParts.map(part => {
+          switch(part.name) {
+            case "Fundamentals":
+              return <li key={part.name}>{part.name} {part.description} {part.exerciseCount}</li>;
+            case "Using props to pass data":
+              return <li key={part.name}>{part.name} {part.groupProjectCount} {part.exerciseCount}</li>;
+            case "Deeper type usage":
+              return <li key={part.name}>{part.name} {part.exerciseSubmissionLink} {part.description} {part.exerciseCount}</li>;
+            case "Exercise 9.15 CoursePart":
+              return <li key={part.name}>{part.name} {part.description} {part.exerciseCount}</li>;
+            default:
+              return assertNever(part);
+          }
+      }
+      )}
     </ul>
   );
 };
--- part9/course-information-typescript/src/types.ts
+++ part9/course-information-typescript/src/types.ts
@@ -1,5 +1,47 @@
 
-export interface CoursePart {
-    name: string;
-    exerciseCount: number;
+// export interface CoursePart {
+//     name: string;
+//     exerciseCount: number;
+// }
+// new types
+export interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
 }
+
+export interface CoursePartWithDescription extends CoursePartBase {
+    description: string;
+}
+
+export interface CoursePartOne extends CoursePartWithDescription {
+  name: "Fundamentals";
+}
+
+export interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+export interface CoursePartThree extends CoursePartWithDescription {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+export interface CoursePartFour extends CoursePartWithDescription {
+  name: "Exercise 9.15 CoursePart";
+}
+
+export type CoursePart =
+    CoursePartOne
+    | CoursePartTwo
+    | CoursePartThree
+    | CoursePartFour;
+
+/**
+ * Helper function for exhaustive type checking
+ */
+export const assertNever = (value: never): never => {
+    throw new Error(
+        `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+};
